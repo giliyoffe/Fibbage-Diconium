4a7ced8edf35bac301205dece3e92ca24337ccc2
Timothy Le Bon <tlebon@gmail.com>
2021-01-22 19:04:48 +0100

    chore: adding production flag for node

--- server1/index.js
+++ server1/index.js
@@ -14,6 +14,15 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, 'client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
+}
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
