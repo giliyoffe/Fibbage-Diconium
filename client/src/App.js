5c2225c0c91e6cf984f0eb5c71e7d6a59e11a983
Hemin Johnson <achayan1992@gmail.com>
2021-01-28 14:06:46 +0100

    feat: add score component

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,19 @@
-import React from 'react';
+import React from "react";
 
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import Join from './components/Join/Join';
-import Chat from './components/Chat/Chat';
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Join from "./components/Join/Join";
+import Chat from "./components/Chat/Chat";
+import Score from './components/Score/Score'
 
 const App = () => (
-    <Router>
-        <Route path="/" exact component={Join} />
-        <Route path="/chat" exact component={Chat} />
-    </Router>
+  <Router>
+    <Route path="/" exact component={Join} />
+    <Route path="/chat" exact component={Chat} />
+    <Route path="/scoreBoard" exact component={Score} />
+  </Router>
 );
 
 export default App;
